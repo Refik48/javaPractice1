970357de011abe64c7f1a2eb522f7c29c1e926ec
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-04-04 23:15:08 +0300

    Initial commit

---
+++ src/day01/Soru03.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class Soru03 {
+
+	public static void main(String[] args) {
+		/*
+		Kullanicidan boyunu cm olarak, kilosunu kilogram olarak alin ve vucut kutle endeksini bulun.  
+		Sonucu tamsayi ve ondalikli sayi olarak yazdirin
+           VKE= kilo/(boy*boy)  …….. Kilo : kilogram , Boy : metre olmalidir
+           
+           
+        Ornek :
+       Input  :  boy :180 kilo : 80
+       Output :  Vucut kutle endeksiniz : 24
+       Vucut kutle endeksiniz : 24.691…
+		
+		 */
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/day02/Soru03.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Soru03 {
+
+	public static void main(String[] args) {
+		/*
+		 
+		Kullanicidan iki tamsayi sayi ve islem cinsini alin ve sayilari kullanicinin 
+		belirledigi isleme tabii tutup sonucu yazdirin.
+         Ornek :
+              Inputs  :  sayilar : 18 , 10   islem : carpma
+              Output :  18 X 10 = 180
+		 */
+
+	}
+
+}
